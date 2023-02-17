ef58e08cad3314a389744d0ace5dcdad0e018004
godgodwinter <kakadz.pro@gmail.com>
2023-02-17 11:14:32 +0700

    update layout

--- src/layouts/admin-layouts-components/AdminNavbar.vue
+++ src/layouts/admin-layouts-components/AdminNavbar.vue
@@ -23,18 +23,6 @@ const toggleSideBar = () => {
 <template>
     <div class="navbar bg-gradient-to-b from-[#d1f6e1] to-white border-b-2 border-gray-50">
         <div class="navbar-start">
-            <!-- <div class="dropdown">
-                                            <label tabindex="0" class="btn btn-ghost lg:hidden">
-                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                                                    stroke="currentColor">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
-                                                </svg>
-                                            </label>
-                                            <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                                                <li><a @click="doLogout()">LOGOUT</a></li>
-
-                                            </ul>
-                                        </div> -->
             <button id="toggleSidebarMobile" aria-expanded="true" aria-controls="sidebar"
                 class="lg:hidden mr-2 text-gray-600 hover:text-gray-900 cursor-pointer p-2 hover:border focus:border focus:ring-2 rounded"
                 @click="toggleSideBar()">
@@ -51,12 +39,9 @@ const toggleSideBar = () => {
                         clip-rule="evenodd" />
                 </svg>
             </button>
-            <span class="btn btn-ghost normal-case text-xl"> YPMT </span>
+            <span class="btn btn-ghost normal-case text-xl"> YPMT ADMINISTRATOR </span>
         </div>
         <div class="navbar-center hidden lg:flex">
-            <ul class="menu menu-horizontal px-1">
-                <li><a @click="doLogout()">LOGOUT</a></li>
-            </ul>
         </div>
         <div class="navbar-end space-x-2">
         </div>
--- src/layouts/admin-layouts-components/AdminSidebar.vue
+++ src/layouts/admin-layouts-components/AdminSidebar.vue
@@ -4,6 +4,7 @@ import { computed } from "vue";
 import { useRouter, useRoute } from "vue-router";
 import { ref } from "vue";
 import { useAdminPagesStore } from "../../stores/admin/adminPagesStore";
+import API from "@/services/adminAuthServices";
 const adminPagesStore = useAdminPagesStore();
 const router = useRouter();
 
@@ -25,6 +26,14 @@ const pagesActiveClass = ref(
 const toggleSideBar = () => {
     adminPagesStore.toggleSideBar();
 }
+const doLogout = async () => {
+    if (confirm("Apakah anda yakin untuk Logout ?")) {
+        const res = await API.doLogout();
+        if (res === true) {
+            router.push({ name: "Login" });
+        }
+    }
+};
 </script>
 <template>
     <aside :class="{ hidden: !isSideBarActive }" id="sidebar"
@@ -34,20 +43,15 @@ const toggleSideBar = () => {
             <div class="flex-1 flex flex-col  pb-4 overflow-y-auto">
                 <div class="flex-1 px-3 space-y-1">
                     <ul class="space-y-1 pb-2 lg:flex flex-wrap px-2 gap-0 justify-between">
-                        <li class="lg:w-full py-0" :class="[pagesActive == 'dashboard' ? pagesActiveClass : '']">
-                            <router-link :to="{ name: 'admin-dashboard' }"
-                                class="text-base-content font-normal rounded-lg flex items-center p-2 group hover:link">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20"
-                                    fill="currentColor">
-                                    <path
-                                        d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
-                                </svg>
-                                <span class="ml-3">Dashboard </span>
-                            </router-link>
+                        <li class="lg:w-full py-0">
+                            <h3
+                                class="text-base-content font-bold rounded-lg flex items-center pt-4  group hover:link underline">
+                                MASTERING
+                            </h3>
                         </li>
                         <li class="lg:w-full py-0" :class="[pagesActive == 'sekolah' ? pagesActiveClass : '']">
                             <router-link :to="{ name: 'admin-sekolah' }"
-                                class="text-base-content font-normal rounded-lg flex items-center p-2 group hover:link">
+                                class="text-base-content font-normal rounded-lg flex p-2 items-center group hover:link">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20"
                                     fill="currentColor">
                                     <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
@@ -55,6 +59,12 @@ const toggleSideBar = () => {
                                 </svg>
                                 <span class="ml-3">Sekolah </span>
                             </router-link>
+                        </li>
+                        <li class="lg:w-full py-0">
+                            <h3
+                                class="text-base-content font-bold rounded-lg flex items-center pt-4  group hover:link underline">
+                                UJIAN STUDI
+                            </h3>
                         </li>
                         <li class="lg:w-full py-0" :class="[pagesActive == 'sekolah' ? pagesActiveClass : '']">
                             <router-link :to="{ name: 'admin-sekolah' }"
@@ -76,6 +86,34 @@ const toggleSideBar = () => {
                                         clip-rule="evenodd" />
                                 </svg>
                                 <span class="ml-3">Paketsoal Ujian Studi </span>
+                            </router-link>
+                        </li>
+                        <li class="lg:w-full py-0">
+                            <h3
+                                class="text-base-content font-bold rounded-lg flex items-center pt-4  group hover:link underline">
+                                DEVELOPMENT
+                            </h3>
+                        </li>
+                        <li class="lg:w-full py-0" :class="[pagesActive == 'sekolah' ? pagesActiveClass : '']">
+                            <span @click="doLogout()"
+                                class="text-base-content font-normal rounded-lg flex items-center p-2 group hover:link">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20"
+                                    fill="currentColor">
+                                    <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                                        clip-rule="evenodd" />
+                                </svg>
+                                <span class="ml-3">Logout </span>
+                            </span>
+                        </li>
+                        <li class="lg:w-full py-0" :class="[pagesActive == 'dashboard' ? pagesActiveClass : '']">
+                            <router-link :to="{ name: 'admin-dashboard' }"
+                                class="text-base-content font-normal rounded-lg flex items-center p-2 group hover:link">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20"
+                                    fill="currentColor">
+                                    <path
+                                        d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+                                </svg>
+                                <span class="ml-3">Dashboard </span>
                             </router-link>
                         </li>
                     </ul>
