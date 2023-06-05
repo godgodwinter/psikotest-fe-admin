363e44977cc4b7dc1d3b512ace110c9e127fe86d
godgodwinter <kakadz.pro@gmail.com>
2023-06-05 14:55:06 +0700

    add cetak tanpa ttd

--- src/views/admin/ujianstudi/proses/UjianstudiProsesPerSiswa.vue
+++ src/views/admin/ujianstudi/proses/UjianstudiProsesPerSiswa.vue
@@ -214,6 +214,15 @@ const doCetakLess = (id = null, token = moment().format("YYYY-MM-DD")) => {
     );
     // }
 };
+const doCetakLess = (id = null, token = moment().format("YYYY-MM-DD")) => {
+    // if (id === null) {
+    //     Toast.danger("Warning", "Data tidak valid!");
+    // } else {
+    window.open(
+        `${BASE_URL_CETAK}api/guest/ujianstudi/v2/cetak/siswa/${siswa_id.value}/less/tanpa_ttd`
+    );
+    // }
+};
 
 const doRefreshData = () => {
     getData();
@@ -239,6 +248,14 @@ const doRefreshData = () => {
                     </svg>
                 </button>
                 <button class="btn btn-sm btn-warning tooltip" data-tip="CETAK Hasil v2" @click="doCetakLess()">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z" />
+                    </svg>
+                </button>
+                <button class="btn btn-sm btn-warning tooltip" data-tip="CETAK Hasil v2 tanpa ttd"
+                    @click="doCetakLess_tanpa_ttd()">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
                         stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round"
--- src/views/admin/ujianstudi/proses/UjianstudiProsesPerkelas.vue
+++ src/views/admin/ujianstudi/proses/UjianstudiProsesPerkelas.vue
@@ -449,6 +449,15 @@ const doCetakLess = (id = null, token = moment().format("YYYY-MM-DD")) => {
     );
     // }
 };
+const doCetakLess_tanpa_ttd = (id = null, token = moment().format("YYYY-MM-DD")) => {
+    // if (id === null) {
+    // Toast.danger("Info", "Menu belum tersedia!");
+    // } else {
+    window.open(
+        `${BASE_URL_CETAK}api/guest/ujianstudi/v2/cetak/kelas/${getSekolahAktif.value.kelas_id}/less/tanpa_ttd`
+    );
+    // }
+};
 const doExportJawabanSiswa = (id = null, token = moment().format("YYYY-MM-DD")) => {
     // if (id === null) {
     // Toast.danger("Info", "Menu belum tersedia!");
@@ -575,6 +584,16 @@ const doCachingRedisPerKelas = async () => {
                 </div>
                 <div class="space-x-2 space-y-2 shadow-sm py-1">
                     <button class="btn btn-sm btn-warning tooltip" data-tip="CETAK Hasil v2" @click="doCetakLess()">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z" />
+                        </svg>
+                    </button>
+                </div>
+                <div class="space-x-2 space-y-2 shadow-sm py-1">
+                    <button class="btn btn-sm btn-warning tooltip" data-tip="CETAK Hasil v2 tanpa ttd"
+                        @click="doCetakLess_tanpa_ttd()">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
                             stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round"
