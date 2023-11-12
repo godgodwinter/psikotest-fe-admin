61d16f7942a2e9ab9e5e9b095000adc5e9eb3e1e
godgodwinter <kakadz.pro@gmail.com>
2023-11-12 15:02:24 +0700

    update cetak per kelas

--- src/views/admin/ujianstudi/proses/UjianstudiProsesPerkelas.vue
+++ src/views/admin/ujianstudi/proses/UjianstudiProsesPerkelas.vue
@@ -620,7 +620,7 @@ const doCetakReactV2 = (ttd="true") => {
                 </button>
             </div>
             <div class="space-x-2 space-y-0 shadow-sm flex justify-start ">
-                <div class="space-x-2 space-y-2 shadow-sm py-1">
+                <!-- <div class="space-x-2 space-y-2 shadow-sm py-1">
                     <button class="btn btn-sm btn-primary tooltip" data-tip="CETAK Hasil" @click="doCetak()">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
                             stroke="currentColor" stroke-width="2">
@@ -647,7 +647,7 @@ const doCetakReactV2 = (ttd="true") => {
                                 d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z" />
                         </svg>
                     </button>
-                </div>
+                </div> -->
                 <div class="space-x-2 space-y-2 shadow-sm py-1">
                     <button class="btn btn-sm btn-primary tooltip" data-tip="Export Jawaban Siswa"
                         @click="doExportJawabanSiswa()">
@@ -662,7 +662,7 @@ const doCetakReactV2 = (ttd="true") => {
             </div>
             <div class="space-x-2 space-y-0 shadow-sm flex justify-start ">
 
-                <div class="space-x-2 space-y-2 shadow-sm py-1">
+                <!-- <div class="space-x-2 space-y-2 shadow-sm py-1">
                     <button class="btn btn-sm btn-success tooltip" data-tip="CETAK Hasil React"
                         @click="doCetakReact('true')">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
@@ -681,10 +681,10 @@ const doCetakReactV2 = (ttd="true") => {
                                 d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z" />
                         </svg>
                     </button>
-                </div>
+                </div> -->
                 
                 <div class="space-x-2 space-y-2 shadow-sm py-1">
-                    <button class="btn btn-sm btn-primary tooltip" data-tip="CETAK HASIL REACT V2"
+                    <button class="btn btn-sm btn-success tooltip" data-tip="CETAK HASIL REACT V2"
                         @click="doCetakReactV2()">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
                             stroke="currentColor" stroke-width="2">
