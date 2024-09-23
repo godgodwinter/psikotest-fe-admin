91bd639057aac7939234a915959ee1dafbf94817
godgodwinter <kakadz.pro@gmail.com>
2024-09-23 08:45:53 +0700

    update lintas : delete superadmin only

--- src/views/admin/ujianstudi/proses/UjianstudiProsesPerkelas.vue
+++ src/views/admin/ujianstudi/proses/UjianstudiProsesPerkelas.vue
@@ -12,6 +12,9 @@ import { useAdminPagesStore } from '@/stores/admin/adminPagesStore'
 import useClipboard from 'vue-clipboard3'
 import moment from "moment/min/moment-with-locales";
 import localization from "moment/locale/id";
+import { useSuperadminStore } from '@/stores/admin/superadminPagesStore';
+const superadminStore = useSuperadminStore();
+const isSuperadminActive = ref(superadminStore.isSuperadminActive)
 moment.updateLocale("id", localization);
 
 const VITE_API_FE_REACT = import.meta.env.VITE_API_FE_REACT
@@ -615,7 +618,8 @@ const doCetakReactV2 = (ttd = "true") => {
                 <p>Catatan : aktifkan paket pada menu Paketsoal</p>
             </div>
             <div class="space-x-2 shadow-sm">
-                <button class="btn btn-sm btn-error p-2" @click="doDeleteProsesSiswaPerkelas()">
+                <button class="btn btn-sm btn-error p-2" @click="doDeleteProsesSiswaPerkelas()"
+                    v-if="isSuperadminActive">
                     Delete Per Kelas
                 </button>
                 <button class="btn btn-sm btn-info p-2" @click="doGenerateSiswaPerkelas()">
@@ -626,7 +630,8 @@ const doCetakReactV2 = (ttd = "true") => {
                 </button>
             </div>
             <div class="space-x-2 space-y-2 shadow-sm">
-                <button class="btn btn-sm btn-error p-2" @click="doDeleteHasilSiswaPerkelas()">
+                <button class="btn btn-sm btn-error p-2" @click="doDeleteHasilSiswaPerkelas()"
+                    v-if="isSuperadminActive">
                     Delete Hasil Per Kelas
                 </button>
                 <button class="btn btn-sm btn-info p-2" @click="doGenerateHasilSiswaPerkelas()">
@@ -779,18 +784,19 @@ const doCetakReactV2 = (ttd = "true") => {
                                                                 d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                                                         </svg></button>
                                                 </RouterLink>
-                                                <button class="btn btn-sm btn-error tooltip"
-                                                    data-tip="Delete Ujianstudi"
-                                                    @click="doDeleteProsesSiswa(props.row.id, props.row.proses_id)"
-                                                    v-if="props.row.paketsoal_nama">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                                        class="w-6 h-6">
-                                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                                            d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                                                    </svg>
-
-                                                </button>
+                                                <div v-if="isSuperadminActive">
+                                                    <button class="btn btn-sm btn-error tooltip"
+                                                        data-tip="Delete Ujianstudi"
+                                                        @click="doDeleteProsesSiswa(props.row.id, props.row.proses_id)"
+                                                        v-if="props.row.paketsoal_nama">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                            viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                            class="w-6 h-6">
+                                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                                        </svg>
+                                                    </button>
+                                                </div>
                                             </div>
                                         </span>
 
